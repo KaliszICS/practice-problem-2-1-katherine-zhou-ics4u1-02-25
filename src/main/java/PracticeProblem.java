5ff351af8aa88b9a043f1fced9a5a73677913c43
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-04-04 23:58:50 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,43 +5,36 @@ public class PracticeProblem {
 	}
 
 	public static int find(int[] array, int number){
-		int index = -1;
-		int occurances = 0;
-		for (int i = 0; i < array.length; i++){
-            if (array[i] == number){
-                index = i;
-				i = array.length;
-            }
-        }
-        return index;
-    }
-
-	public static String findLast(String[] array, String string){
-		int index=-1;
-		int occurances = 0;
-        for ( int i = array.length- 1; i>= 0; i--){
-            if (array[i].equals(string)){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-	public static char findSecond(char[] array, char character){
-		int index = -1;
-		int counter = 0;
-		for (int i = 0; i < array.length; i++) 
-			if (array[i] == character){
-				counter++;
-				if (counter == 2) {
-					index = i;
+		
+		for (int i = 0; i<array.length; i++){
+			if (array[i] == number){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int findLast(String[] array, String string){
+		int index =-1;
+		for (int i =0; i < array.length; i++){
+			if (array[i].equals(string)){
+				index =i;
+			}
+		}
+		return index;
+	}
+	public static int findSecond(char[] array, char character){
+		int occurence =0;
+		int index =-1;
+		for (int i =0; i<array.length; i++){
+			if (array[i]== character){
+				index=i;
+				occurence++;
+				if (occurence ==2){
+					return index;
 				}
 			}
-			if (counter == 1) {
-				index = -1;
-			}
-		
-			return index;
 		}
+		return index;
+	}
 	}
 
